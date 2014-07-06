6cf7121cfcf991cc763638bfec03939ae84c9b26
Liu Yang <zation1@gmail.com>
2014-07-06 18:51:29 +0800

    init project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name logicMonitorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the logicMonitorApp
+ */
+angular.module('logicMonitorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name logicMonitorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the logicMonitorApp
+ */
+angular.module('logicMonitorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
