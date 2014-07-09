7285bfbb60649aceed60f4f53cd5b843eccc87af
Liu Yang <zation1@gmail.com>
2014-07-09 16:50:22 +0800

    use newThreshold instead of threshold when add

--- app/scripts/directives/thresholdEditor.js
+++ app/scripts/directives/thresholdEditor.js
@@ -17,42 +17,6 @@ angular.module('logicMonitorApp')
         'threshold': '='
       },
       controller: ['$scope', function($scope) {
-        $scope.active = false;
-        $scope.activate = function() {
-          $scope.active = true;
-        };
-        $scope.remove = function($event) {
-          $event.stopPropagation();
-          if ($scope.onRemove) {
-            $scope.onRemove($scope.threshold);
-          }
-        };
-        $scope.cancel = function($event) {
-          $event.stopPropagation();
-          $scope.active = false;
-        };
-        $scope.submit = function($event) {
-          $event.stopPropagation();
-          $scope.active = false;
-
-          if ($scope.onSave) {
-            $scope.onSave({
-              from: $scope.from,
-              until: $scope.until,
-              operator: $scope.operator,
-              numberForWarning: $scope.numberForWarning,
-              numberForError: $scope.numberForError,
-              numberForCritical: $scope.numberForCritical
-            });
-          }
-
-          if ($scope.threshold) {
-            $scope.comparison = $scope.comparisons[0];
-            $scope.from = $scope.until = $scope.numberForWarning =
-              $scope.numberForError = $scope.numberForCritical = undefined;
-          }
-        };
-
         $scope.times = [];
         for (var hour = 0; hour < 24; hour++) {
           for (var minutes = 0; minutes < 60; minutes += 15) {
@@ -76,8 +40,41 @@ angular.module('logicMonitorApp')
             operator: '<'
           }
         ];
-        $scope.operator = $scope.comparisons[0].operator;
 
+        var TYPE_ADD = 'TYPE_ADD', TYPE_EDIT = 'TYPE_EDIT', _type = TYPE_EDIT;
+        if ($scope.threshold === undefined) {
+          _type = TYPE_ADD;
+        }
+        if (_type === TYPE_ADD) {
+          $scope.newThreshold = {
+            operator: $scope.comparisons[0].operator
+          };
+        }
+        $scope.active = false;
+        $scope.activate = function() {
+          $scope.active = true;
+        };
+        $scope.remove = function($event) {
+          $event.stopPropagation();
+          if ($scope.onRemove) {
+            $scope.onRemove($scope.threshold);
+          }
+        };
+        $scope.cancel = function($event) {
+          $event.stopPropagation();
+          $scope.active = false;
+        };
+        $scope.submit = function($event) {
+          $event.stopPropagation();
+          $scope.active = false;
+
+          if (_type === TYPE_ADD) {
+            $scope.onSave($scope.newThreshold);
+            $scope.newThreshold = {
+              operator: $scope.comparisons[0].operator
+            };
+          }
+        };
       }]
     };
   });
