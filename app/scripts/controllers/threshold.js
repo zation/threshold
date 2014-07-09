575088e54c7e2e50e0f3e2a26a79a504cd7f1717
Liu Yang <zation1@gmail.com>
2014-07-09 12:10:40 +0800

    add demo data

--- app/scripts/controllers/threshold.js
+++ app/scripts/controllers/threshold.js
@@ -4,6 +4,40 @@ angular.module('logicMonitorApp')
   .controller('ThresholdCtrl', ['$scope', 'Thresholds',
     function($scope, Thresholds) {
       $scope.thresholds = Thresholds.getInstance();
+      $scope.thresholds.add({
+        from: '123',
+        until: '123',
+        comparison: {
+          name: 'is greater than(>)',
+          operator: '>'
+        },
+        numberForWarning: 11,
+        numberForError: 22,
+        numberForCritical: 0
+      });
+      $scope.thresholds.add({
+        from: '123',
+        until: '123',
+        comparison: {
+          name: 'is greater than(>)',
+          operator: '>'
+        },
+        numberForWarning: 11,
+        numberForError: 22,
+        numberForCritical: 0
+      });
+      $scope.thresholds.add({
+        from: '123',
+        until: '123',
+        comparison: {
+          name: 'is greater than(>)',
+          operator: '>'
+        },
+        numberForWarning: 11,
+        numberForError: 22,
+        numberForCritical: 0
+      });
+      
       $scope.onAddComplete = function(threshold) {
         $scope.thresholds.add(threshold);
       };
