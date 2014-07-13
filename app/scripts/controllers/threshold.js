915c106994065ae00221169e73ca0172a8f4985a
Liu Yang <zation1@gmail.com>
2014-07-13 21:44:50 +0800

    init thresholds with codes

--- app/scripts/controllers/threshold.js
+++ app/scripts/controllers/threshold.js
@@ -1,41 +1,14 @@
 'use strict';
 
 angular.module('logicMonitorApp')
-  .controller('ThresholdCtrl', ['$scope', 'Thresholds', 'Threshold',
-    function($scope, Thresholds, Threshold) {
+  .controller('ThresholdCtrl', ['$scope', 'Thresholds',
+    function($scope, Thresholds) {
       $scope.thresholds = Thresholds.getInstance();
-      $scope.thresholds.addA(new Threshold({
-        from: '',
-        until: '',
-        operator: '>',
-        numberForWarning: '',
-        numberForError: 22,
-        numberForCritical: 0
-      }));
-      $scope.thresholds.addA(new Threshold({
-        from: '22:00',
-        until: '02:00',
-        operator: '>',
-        numberForWarning: '',
-        numberForError: 22,
-        numberForCritical: 0
-      }));
-      $scope.thresholds.addA(new Threshold({
-        from: '00:30',
-        until: '01:30',
-        operator: '=',
-        numberForWarning: 11,
-        numberForError: '',
-        numberForCritical: 0
-      }));
-      $scope.thresholds.addA(new Threshold({
-        from: '03:30',
-        until: '04:30',
-        operator: '<',
-        numberForWarning: 11,
-        numberForError: 12312,
-        numberForCritical: 0
-      }));
+      $scope.codes = '() > 0 22 0, ' +
+        '(22:00 02:00) > 0 22 0, ' +
+        '(00:30 01:30) = 11 0 0, ' +
+        '(03:30 04:30) < 11 12 0';
+      $scope.thresholds.updateWith($scope.codes);
 
       $scope.$on('activate', function(event, id) {
         $scope.$broadcast('deactivate', id);
--- app/scripts/services/thresholds.js
+++ app/scripts/services/thresholds.js
@@ -1,39 +1,74 @@
 'use strict';
 
 angular.module('logicMonitorApp')
-  .factory('Thresholds', ['$collection', function($collection) {
-    var Thresholds = $collection;
-    var COLORS = ['purple', 'light-blue', 'green', 'blue', 'magenta'];
+  .factory('Thresholds', ['$collection', 'Threshold',
+    function($collection, Threshold) {
+      var Thresholds = $collection;
+      var COLORS = ['purple', 'light-blue', 'green', 'blue', 'magenta'];
 
-    Thresholds.TIMES = [];
-    for (var hour = 0; hour < 24; hour++) {
-      for (var minutes = 0; minutes < 60; minutes += 15) {
-        var hourString = hour < 10 ? '0' + hour : hour;
-        var minutesString = minutes < 10 ? '0' + minutes : minutes;
-        Thresholds.TIMES.push(hourString + ':' + minutesString);
+      Thresholds.TIMES = [];
+      for (var hour = 0; hour < 24; hour++) {
+        for (var minutes = 0; minutes < 60; minutes += 15) {
+          var hourString = hour < 10 ? '0' + hour : hour;
+          var minutesString = minutes < 10 ? '0' + minutes : minutes;
+          Thresholds.TIMES.push(hourString + ':' + minutesString);
+        }
       }
-    }
 
-    Thresholds.COMPARISONS = [
-      {
-        name: 'is equal to (=)',
-        operator: '='
-      },
-      {
-        name: 'is greater than (>)',
-        operator: '>'
-      },
-      {
-        name: 'is less than (<)',
-        operator: '<'
-      }
-    ];
+      Thresholds.COMPARISONS = [
+        {
+          name: 'is equal to (=)',
+          operator: '='
+        },
+        {
+          name: 'is greater than (>)',
+          operator: '>'
+        },
+        {
+          name: 'is less than (<)',
+          operator: '<'
+        }
+      ];
 
-    Thresholds.prototype.addA = function(threshold) {
-      var index = this.array.length % COLORS.length;
-      threshold.color = COLORS[index];
-      this.add(threshold);
-    };
+      Thresholds.prototype.updateWith = function(codes) {
+        var thresholds = this;
+        angular.forEach(codes.split(','), function(code) {
+          code = code.trim().split(')');
+          var times = code[0].substr(1).split(' ');
+          var from = '';
+          var until = '';
+          if (times.length > 1) {
+            from = times[0];
+            until = times[1];
+          }
+          var operatorAndAlertNumbers = code[1].trim().split(' ');
+          var operator = operatorAndAlertNumbers.shift();
+          var alertNumbers = operatorAndAlertNumbers;
+          var numberArray = [0, 0, 0]
+          angular.forEach(alertNumbers, function(alertNumber, index) {
+            alertNumber = Number(alertNumber);
+            if (isNaN(alertNumber)) {
+              return;
+            }
+            numberArray[index] = alertNumber;
+          });
 
-    return Thresholds;
-  }]);
+          thresholds.addA(new Threshold({
+            from: from,
+            until: until,
+            operator: operator,
+            numberForWarning: numberArray[0],
+            numberForError: numberArray[1],
+            numberForCritical: numberArray[2]
+          }));
+        });
+      };
+
+      Thresholds.prototype.addA = function(threshold) {
+        var index = this.array.length % COLORS.length;
+        threshold.color = COLORS[index];
+        this.add(threshold);
+      };
+
+      return Thresholds;
+    }]);
